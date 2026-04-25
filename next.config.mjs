380cb63af054c7155185b51454faf1aed0768150
Global Digital Care <info.globaldigitalcare@gmail.com>
2026-04-25 15:44:36 +0500

    update version

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: __dirname,
   },
 };
 
